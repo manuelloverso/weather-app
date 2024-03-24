940ed083f06800165fe1ae4af1d4806969b75287
manuelloverso <manuelloverso@gmail.com>
2024-03-24 19:53:09 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import WeatherApp from "./components/WeatherApp.vue";
+
+export default {
+  name: "App",
+  components: {
+    WeatherApp,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <WeatherApp />
+</template>
+
+<style scoped></style>
---
+++ src/components/WeatherApp.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "WeatherApp",
+  data() {
+    return {
+      hello: "hello world",
+    };
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+<template></template>
+<style scoped></style>
